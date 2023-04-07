1032fb7bff642a8039c7ad51dd70d265b369f95f
irregularunit <pawsfiction@gmail.com>
2023-04-07 15:12:55 +0200

    Reduces the memory overhead

--- __main__.py
+++ __main__.py
@@ -1,30 +1,25 @@
 import argparse
+import io
 import os
 
 from .bottom import from_bottom, to_bottom
 
 parser = argparse.ArgumentParser(
-    description='Tool for translating between bottom and human readable text'
+    description="Tool for translating between bottom and human readable text"
 )
-parser.add_argument(
-    '--from-bottom',
-    default=False,
-    const=True,
-    action='store_const'
-)
-if os.isatty(0):
-    parser.add_argument('text')
+parser.add_argument("--from-bottom", default=False, const=True, action="store_const")
+parser.add_argument("text", nargs="*", default=None, help="Text to translate")
 
 args = parser.parse_args()
 
 while True:
     try:
-        text = args.text or input()
+        text = "".join(args.text) if args.text else input()
 
         if args.from_bottom:
-            print(from_bottom(text))
+            print(from_bottom(io.StringIO(text)))
         else:
-            print(to_bottom(text))
+            print(to_bottom(io.StringIO(text)))
 
         if os.isatty(0):
             break
